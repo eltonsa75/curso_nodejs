6528c6707fe9c6fd48c08307c6e5388668b4a472
Elton <eltonsa75@hotmail.com>
2021-07-29 19:52:19 -0300

    Criando o script de conexão com o banco de dado Mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,4 +1,15 @@
 module.exports = function(app){
+
+    var mysql = require('mysql');
+
+    var connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : 'hodel',
+        database : 'portal_noticias'
+
+    });
+
     app.get('/noticias', function(req, res){
         res.render("noticias/noticias");
     });
