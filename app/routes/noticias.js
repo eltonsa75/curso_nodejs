7ab91dc3324fa70118c537ab046d708c0bdc1597
Elton <eltonsa75@hotmail.com>
2021-07-30 17:08:14 -0300

    Criando o Script dbConnection

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,21 +1,14 @@
+const dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
+
+    var connection = dbConnection();
 
     app.get('/noticias', function(req, res){
 
-    var mysql = require('mysql');
-
-    var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : 'hodel',
-        database : 'portal_noticias'
-
-    });
-
     connection.query('select * from noticias', function(error, result){
-            res.send(result);
+            res.render("noticias/noticias",{noticias: result});
     });
 
-        //res.render("noticias/noticias");
     });
 };
